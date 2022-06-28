4642f10b5ad97c79dfb52147ea61305b298aa7f8
phillip shields <phillip.d.shields1382@gmail.com>
2022-06-28 12:00:05 +1200

    added category model

---
+++ Shared/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+    class Category
+    {
+        [Key]
+        public int CategoryId { get; set; } 
+       
+        [Required]
+        [MaxLength(256)]
+        public string ThumbnailImgPath { get; set; }
+        
+        [Required]
+        [MaxLength(125)]
+        public string Name { get; set; }
+        
+        [Required]
+        [MaxLength(256)]
+        public string Description { get; set; }
+
+    }
+}
