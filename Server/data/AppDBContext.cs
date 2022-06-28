ed761ea59d7ab87355921d7fc9da08e4d669ed7c
phillip shields <phillip.d.shields1382@gmail.com>
2022-06-28 12:19:00 +1200

    AppDBContext added

---
+++ Server/data/AppDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace Server.data
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // call base version of this method, else get an error
+            base.OnModelCreating(modelBuilder);
+
+            Category[] categoriesToSeed = new Category[3];
+
+            for (int i = 1; i < 4; i++)
+            {
+                categoriesToSeed[i - 1] = new Category
+                {
+                    CategoryId = i,
+                    ThumbnailImgPath = "uploads.placeholder.jpg",
+                    Name = $"Category {i}",
+                    Description = $"description of category {i}"
+                };
+            }
+            modelBuilder.Entity<Category>().HasData(categoriesToSeed);
+        }
+    }
+}
