efccbe76551ef1edb13ec1afb072530630daccda
phillip shields <phillip.d.shields1382@gmail.com>
2022-06-28 22:21:42 +1200

    DBContext configured

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Server.data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString(name: "DefaultConnection"
+        ));
+});
+
+builder.Services.AddRouting(configureOptions: options => options.LowercaseUrls = true);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setupAction: c =>
+{
+    c.SwaggerDoc(name: "v1", info: new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Server", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -17,6 +31,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
