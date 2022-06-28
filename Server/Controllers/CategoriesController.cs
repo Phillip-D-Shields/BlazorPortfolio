16eecff5d1c988820163f0a0d3507abd32cb2eee
phillip shields <phillip.d.shields1382@gmail.com>
2022-06-29 10:15:57 +1200

    added first api get

---
+++ Server/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.data;
+using Shared.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        // localhost:5001/api/categories
+
+        private readonly AppDBContext _appDBContext;
+
+        public CategoriesController(AppDBContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+
+            List<Category> categories = await _appDBContext.Categories.ToListAsync();
+
+            return Ok(categories);
+        }
+    }
+}
