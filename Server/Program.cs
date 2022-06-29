a07ce9299a96b58a57602216df188497211fcb14
phillip shields <phillip.d.shields1382@gmail.com>
2022-06-30 10:49:16 +1200

    namespace issue

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Server.Data;
+using Server.data;
 
 var builder = WebApplication.CreateBuilder(args);
 
