6416d6dda1b38a8ca4eecf66f10fcf758abb69c4
phillip shields <phillip.d.shields1382@gmail.com>
2022-06-30 12:34:00 +1200

    updated static api endpoint

--- Client/Static/APIEndpoints.cs
+++ Client/Static/APIEndpoints.cs
@@ -5,7 +5,7 @@
 #if DEBUG
         internal const string ServerBaseUrl = "https://localhost:5003";
 #else
-        internal const string ServerBaseUrl = "https://appname.azurewebsite.net";
+        internal const string ServerBaseUrl = "https://pdsportfolioserver.azurewebsites.net/";
 #endif
 
         internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
